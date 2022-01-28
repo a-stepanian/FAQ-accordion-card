4b76d9ddc42c38ce6a34f127f772d73975b48546
Alex Stepanian <alexstepanian11@gmail.com>
2022-01-28 12:25:38 -0800

    create loop to add event listener to all questions

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const question1 = document.querySelectorAll('.question')[0];
-const arrow1 = document.querySelectorAll('.arrow')[0];
-const answer1 = document.querySelectorAll('.answer')[0];
-
-question1.addEventListener('click', () => {
-    question1.classList.toggle('clicked');
-    arrow1.classList.toggle('flip');
-    answer1.classList.toggle('expand');
-})
+for (let i = 0; i < 5; i++) {
+    const question = document.querySelectorAll('.question')[i];
+    const arrow = document.querySelectorAll('.arrow')[i];
+    const answer = document.querySelectorAll('.answer')[i];
+    
+    question.addEventListener('click', () => {
+        question.classList.toggle('clicked');
+        arrow.classList.toggle('flip');
+        answer.classList.toggle('expand');
+    })  
+}
